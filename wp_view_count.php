78defef68f9d316f47db68b15a906425b3d8e149
Szymon Bedeniczuk <01158593@pw.edu.pl>
2025-06-17 17:51:06 +0200

    First commit, creation of the main file of the plug in

---
+++ wp_view_count.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: WP View Count
+ * Plugin URI: https://github.com/azmazm/wp-view-count
+ * Description: A plug in for WordPress which allows for counting and displaying the number of views on posts
+ * Version: 1.0.0
+ * Author: Szymon Bedeniczuk
+ */
+
+?>
