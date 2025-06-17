7f66ec2ee1e90770f9de29174e678e9c2f492bed
Szymon Bedeniczuk <01158593@pw.edu.pl>
2025-06-17 20:10:39 +0200

    adding main function of the plug in, collecting cookie data and adding shortcode for displaying it

--- wp_view_count.php
+++ wp_view_count.php
@@ -7,4 +7,40 @@
  * Author: Szymon Bedeniczuk
  */
 
+ function count_views(){
+    if(!is_single()) return;
+    global $post;
+    $post_id = $post->ID;
+    $cookie_id = $post_id . '_visited';
+    $visited = isset($_COOKIE[$cookie_id]) ? $_COOKIE[$cookie_id] : false;
+    if($visited){
+        return;
+    }
+
+    $views = get_post_meta($post_id, 'views', true);
+    
+    if(!is_numeric($views)){
+        delete_post_meta($post_id, 'views');
+        add_post_meta($post_id, 'views', '1');
+    }else{
+        $views++;
+        update_post_meta($post_id, 'views', $views);
+    }
+
+    setcookie($cookie_id, 'visited', time() + 3600, '/');
+ }
+
+ add_action('init', 'count_views');
+
+ function get_views(){
+    global $post;
+    $post_id = $post->ID;
+    $views = get_post_meta($post_id, 'views', true);
+    return $views;
+}
+
+function wp_view_count(){
+    return "Wyświetlenia: " . get_views();
+}
+add_shortcode('wp_view_count', 'wp_view_count');
 ?>
