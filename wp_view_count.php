c561f0f5a8bd2df0c3699303883173c860d3028a
Szymon Bedeniczuk <01158593@pw.edu.pl>
2025-06-18 00:40:22 +0200

    adding plugin page in tools submenu, fixed a major error in add_action for count_views() method, it would not count the views due to the method being added to early in the program life cycle

--- wp_view_count.php
+++ wp_view_count.php
@@ -30,7 +30,7 @@
     setcookie($cookie_id, 'visited', time() + 3600, '/');
  }
 
- add_action('init', 'count_views');
+ add_action('wp', 'count_views');
 
  function get_views(){
     global $post;
@@ -40,7 +40,82 @@
 }
 
 function wp_view_count(){
-    return "Wyświetlenia: " . get_views();
+    return "Views: " . get_views();
 }
 add_shortcode('wp_view_count', 'wp_view_count');
+
+
+add_action('admin_menu', 'wp_view_count_add_page');
+
+function wp_view_count_add_page() {
+    add_management_page(
+        'Posts Views',        
+        'Posts Views',        
+        'manage_options',   
+        'wp_view_count',      
+        'wp_view_count_page' 
+    );
+}
+
+function wp_view_count_reset_views() {
+    if (
+        isset($_POST['wp_view_count_reset_views']) &&
+        isset($_POST['post_id']) &&
+        current_user_can('manage_options')
+    ) {
+        $post_id = intval($_POST['post_id']);
+        delete_post_meta($post_id, 'views');
+        add_post_meta($post_id, 'views', 0);
+        echo '<div class="notice notice-success is-dismissible"><p>Views were successfully reset.</p></div>';
+    }
+}
+
+add_action('admin_init', 'wp_view_count_reset_views');
+
+function wp_view_count_page() {
+    $args = [
+        'post_type'      => 'post',
+        'posts_per_page' => 5,
+        'orderby'        => 'meta_value_num',
+        'meta_key'       => 'views',
+        'order'          => 'DESC',
+    ];
+
+    $posts = new WP_Query($args);
+
+    echo '<div class="wrap">';
+    echo '<h2>5 most popular posts</h2>';
+
+    if ($posts->have_posts()) {
+        echo '<table class="widefat fixed striped">';
+        echo '<thead><tr><th>Title</th><th>Views</th><th>Actions</th></tr></thead><tbody>';
+
+        while ($posts->have_posts()) {
+            $posts->the_post();
+            $post_id = get_the_ID();
+            $views = get_post_meta($post_id, 'views', true);
+            $views = is_numeric($views) ? $views : 0;
+
+            echo '<tr>';
+            echo '<td><a href="' . get_permalink($post_id) . '" target="_blank">' . get_the_title() . '</a></td>';
+            echo '<td>' . $views . '</td>';
+            echo '<td>
+                    <a href="' . get_edit_post_link($post_id) . '" class="button">Edit Post</a>
+                    <form method="post" style="display:inline;">
+                        <input type="hidden" name="post_id" value="' . esc_attr($post_id) . '">
+                        <input type="submit" name="wp_view_count_reset_views" class="button button-secondary" value="Reset Views">
+                    </form>
+                  </td>';
+            echo '</tr>';
+        }
+
+        echo '</tbody></table>';
+    } else {
+        echo '<p>No posts to display.</p>';
+    }
+
+    echo '</div>';
+    wp_reset_postdata();
+}
+
 ?>
