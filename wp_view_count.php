a79a8d772bd390c3fe98f80dcc3b62e8778ccae3
Szymon Bedeniczuk <01158593@pw.edu.pl>
2025-06-18 16:00:30 +0200

    Added menu page, with top 5 posts and option to edit and reset views on the posts. Added security for the variables, and option for translation of the text of the plugin

--- wp_view_count.php
+++ wp_view_count.php
@@ -2,16 +2,18 @@
 /**
  * Plugin Name: WP View Count
  * Plugin URI: https://github.com/azmazm/wp-view-count
- * Description: A plug in for WordPress which allows for counting and displaying the number of views on posts
+ * Description: A plugin for WordPress which allows counting and displaying the number of views on a given post with a simple shortcode
  * Version: 1.0.0
  * Author: Szymon Bedeniczuk
  */
+
+ defined('ABSPATH') || exit;
 
  function count_views(){
     if(!is_single()) return;
     global $post;
     $post_id = $post->ID;
-    $cookie_id = $post_id . '_visited';
+    $cookie_id = 'wp_view_count_' . $post_id . '_visited';
     $visited = isset($_COOKIE[$cookie_id]) ? $_COOKIE[$cookie_id] : false;
     if($visited){
         return;
@@ -40,7 +42,7 @@
 }
 
 function wp_view_count(){
-    return "Views: " . get_views();
+    return esc_html__('Views: ', 'wp_view_count') . get_views();
 }
 add_shortcode('wp_view_count', 'wp_view_count');
 
@@ -49,8 +51,8 @@ add_action('admin_menu', 'wp_view_count_add_page');
 
 function wp_view_count_add_page() {
     add_management_page(
-        'Posts Views',        
-        'Posts Views',        
+        __('Posts Views', 'wp_view_count'),
+        __('Posts Views', 'wp_view_count'),       
         'manage_options',   
         'wp_view_count',      
         'wp_view_count_page' 
@@ -64,9 +66,8 @@ function wp_view_count_reset_views() {
         current_user_can('manage_options')
     ) {
         $post_id = intval($_POST['post_id']);
-        delete_post_meta($post_id, 'views');
-        add_post_meta($post_id, 'views', 0);
-        echo '<div class="notice notice-success is-dismissible"><p>Views were successfully reset.</p></div>';
+        update_post_meta($post_id, 'views', 0);
+        echo '<div class="notice notice-success is-dismissible"><p>' . esc_html__('Views have been reset.', 'wp_view_count') . '</p></div>';
     }
 }
 
@@ -84,11 +85,11 @@ function wp_view_count_page() {
     $posts = new WP_Query($args);
 
     echo '<div class="wrap">';
-    echo '<h2>5 most popular posts</h2>';
+    echo '<h2>' . esc_html__('5 Most Popular Posts', 'wp_view_count') . '</h2>';
 
     if ($posts->have_posts()) {
         echo '<table class="widefat fixed striped">';
-        echo '<thead><tr><th>Title</th><th>Views</th><th>Actions</th></tr></thead><tbody>';
+        echo '<thead><tr><th>' . esc_html__('Title', 'wp_view_count') . '</th><th>' . esc_html__('Views', 'wp_view_count') . '</th><th>' . esc_html__('Actions', 'wp_view_count') . '</th></tr></thead><tbody>';
 
         while ($posts->have_posts()) {
             $posts->the_post();
@@ -97,13 +98,13 @@ function wp_view_count_page() {
             $views = is_numeric($views) ? $views : 0;
 
             echo '<tr>';
-            echo '<td><a href="' . get_permalink($post_id) . '" target="_blank">' . get_the_title() . '</a></td>';
-            echo '<td>' . $views . '</td>';
+            echo '<td><a href="' . esc_url(get_permalink($post_id)) . '" target="_blank">' . esc_html(get_the_title()) . '</a></td>';
+            echo '<td>' . esc_html($views) . '</td>';
             echo '<td>
-                    <a href="' . get_edit_post_link($post_id) . '" class="button">Edit Post</a>
+                    <a href="' . esc_url(get_edit_post_link($post_id)) . '" class="button">' . esc_html__('Edit Post' , 'wp_view_count') . '</a>
                     <form method="post" style="display:inline;">
                         <input type="hidden" name="post_id" value="' . esc_attr($post_id) . '">
-                        <input type="submit" name="wp_view_count_reset_views" class="button button-secondary" value="Reset Views">
+                        <input type="submit" name="wp_view_count_reset_views" class="button button-secondary" value= ' . esc_attr__('Reset Views', 'wp_view_count') . '>
                     </form>
                   </td>';
             echo '</tr>';
@@ -111,7 +112,7 @@ function wp_view_count_page() {
 
         echo '</tbody></table>';
     } else {
-        echo '<p>No posts to display.</p>';
+        echo '<p>' . esc-html__('No posts to display', 'wp_view_count') . '.</p>';
     }
 
     echo '</div>';
